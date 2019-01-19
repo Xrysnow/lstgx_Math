d9493b3b74af376be29be5257afab0d964ac31cf
Xrysnow <xrysnow@outlook.com>
2019-01-19 18:45:20 +0800

    fix

--- XConstant.h
+++ XConstant.h
@@ -9,6 +9,8 @@ namespace xmath
 	constexpr double pi = 3.14159265358979323846;
 	constexpr double pi_2 = pi / 2.0;
 	constexpr double pi_4 = pi / 4.0;
+	constexpr double pix2 = pi * 2.0;
+	constexpr double pix4 = pi * 4.0;
 	constexpr double pix2_3 = pi * 2 / 3;
 	constexpr double pix4_3 = pi * 4 / 3;
 }
